294b3b1bad5530647df2ff21e74a63d46ec832a6
oddCompost <richomas@hotmail.com>
2022-04-29 11:33:48 +0100

    Classes created

--- logic.js
+++ logic.js
@@ -1,4 +1,84 @@
 
+/************************************
+
+Scooter Hire App
+By
+Amenity42 / oddCompost
+29 - 04 - 2022
+
+************************************/
 
 
-console.log('Hello World');
+
+
+//* --------------------------| Classes |--------------------------
+
+class User{
+
+      constructor(Name, Age){
+
+            this.name = Name;
+            this.age = Age;
+
+      }
+
+      name;
+      age;
+      scooter;
+
+}
+
+class Scooter{
+
+      constructor(){
+            
+            this.chargeRemaining = 100;
+            this.needsMaintinance = false;
+            this.ID = Math.random() * 100;
+
+      }
+
+      chargeRemaining;
+      needsMaintinance;
+      ID;
+      location;
+
+}
+
+class ChargeStation{
+
+      constructor(){
+            
+      }
+
+      scooters = [];
+      location;
+      ID;
+
+      chargeScooter(){
+            //todo: Charge scooter 
+      }
+
+}
+
+class Maintinance{
+
+      constructor(){
+            
+      }
+
+      fixScooter(scooter){
+            //todo Fix scooter
+      }
+
+}
+
+class Payment{
+
+      constructor(){
+
+      }
+      
+}
+
+//* --------------------------|   End   |--------------------------
